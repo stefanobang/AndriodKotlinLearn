f4bde2b321e61c514224ebbb7163b8c5afbc064d
stefanobang <stepanobang1@hotmail.com>
2022-01-13 15:49:57 +0900

    Learning how to use Kotlin (First Code Upload to Github)

--- app/src/main/java/org/techtown/andriodlab/Constructor.kt
+++ app/src/main/java/org/techtown/andriodlab/Constructor.kt
@@ -1,5 +1,5 @@
 package org.techtown.andriodlab
-//보저 생성자 선언
+//보조 생성자 선언
 class ConstructorLearn{
     constructor(name: String){
         println("contructor(name: String) call..")
@@ -9,8 +9,20 @@ class ConstructorLearn{
     }
 }
 
+/*
+class User(name: String){
+    constructor(name: String){ //=> 오류!! 반드시 주랑 보조 사용하면 연결해야함!
+        .....
+ */
+class bothConstructor(name: String) {
+    constructor(name: String, count: Int):this(name) {
+        println("bothConstructor(name: String) and constructor(name: String, count: Int):this(name)....")
+    }
+}
+
 fun main() {
     val user1 = ConstructorLearn("Stef1")
     val user2 = ConstructorLearn("stef2",10)
+    val user3 = bothConstructor("stef3",30)
 
 }
